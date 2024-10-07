f5affc9b638c6da399bbeddcb79dee67960af80e
ThomasB98 <bansodthomas@gmail.com>
2024-10-07 17:16:16 +0530

    [Thomas]ADD: USER & NOTE entity

---
+++ ModelLayer/Model/Entity/Note.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelLayer.Model.Entity
+{
+    /// <summary>
+    /// Represents a note in the system.
+    /// This class contains all note-related information and its relationship to a user.
+    /// </summary>
+    [Table("Notes")]
+    public class Note
+    {
+        /// <summary>
+        /// Gets or sets the unique identifier for the note.
+        /// </summary>
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]   
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the title of the note.
+        /// </summary>
+        [Required]
+        [StringLength(200)]
+        public string Title { get; set; }
+
+        /// <summary>
+        /// Gets or sets the content of the note.
+        /// </summary>
+        [Required]
+        public string Content { get; set; }
+
+        /// <summary>
+        /// Gets or sets the color of the note for display purposes.
+        /// </summary>
+        [StringLength(50)]
+        public string Color { get; set; }
+
+        /// <summary>
+        /// Gets or sets whether the note is pinned for quick access.
+        /// </summary>
+        public bool IsPinned { get; set; }
+
+        /// <summary>
+        /// Gets or sets whether the note is archived.
+        /// </summary>
+        public bool IsArchived { get; set; }
+
+        /// <summary>
+        /// Gets or sets whether the note is marked as deleted.
+        /// </summary>
+        public bool IsDeleted { get; set; }
+
+        /// <summary>
+        /// Gets or sets the date when the note was created.
+        /// </summary>
+        [Required]
+        [DataType(DataType.DateTime)]
+        public DateTime CreatedDate { get; set; }
+
+        /// <summary>
+        /// Gets or sets the date when the note was last modified.
+        /// </summary>
+        [Required]
+        [DataType(DataType.DateTime)]
+        public DateTime ModifiedDate { get; set; }
+
+        /// <summary>
+        /// Gets or sets the ID of the user who owns this note.
+        /// This is a foreign key to the Users table.
+        /// </summary>
+        [Required]
+        public int UserId { get; set; }
+
+        /// <summary>
+        /// Navigation property for the user who owns this note.
+        /// This represents the many-to-one relationship between Note and User.
+        /// </summary>
+        [ForeignKey("UserId")]
+        public User User { get; set; }
+    }
+}
---
+++ ModelLayer/Model/Entity/User.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelLayer.Model.Entity
+{
+    /// <summary>
+    /// Represents a user in the system.
+    /// This class contains all user-related information and relationships.
+    /// </summary>
+    [Table("Users")]
+    public class User
+    {
+        /// <summary>
+        /// Gets or sets the unique identifier for the user.
+        /// </summary>
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the user's full name.
+        /// </summary>
+        [Required]
+        [StringLength(100, MinimumLength = 2)]
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the user's email address.
+        /// </summary>
+        [Required]
+        [EmailAddress]
+        [StringLength(255)]
+        public string Email { get; set; }
+
+        /// <summary>
+        /// Gets or sets the hashed password for the user.
+        /// This should never store the actual password.
+        /// </summary>
+        [Required]
+        public string PasswordHash { get; set; }
+
+        /// <summary>
+        /// Gets or sets the date when the user account was created.
+        /// </summary>
+        [Required]
+        [DataType(DataType.DateTime)]
+        public DateTime CreatedDate { get; set; }
+
+        // <summary>
+        /// Gets or sets whether the user account is active.
+        /// Inactive accounts cannot access the system.
+        /// </summary>
+        [Required]
+        public bool IsActive { get; set; }
+
+        /// <summary>
+        /// Gets or sets the collection of notes associated with this user.
+        /// This is a navigation property for the one-to-many relationship between User and Note.
+        /// </summary>
+        [InverseProperty("User")]
+        public ICollection<Note> Notes { get; set; } = new List<Note>();
+    }
+}
